86eb9b6abca05f06f2ad98c66a758fc3a9b9fb5d
revoX <67857989+revoX-Development@users.noreply.github.com>
2021-03-12 13:10:41 +0100

    Level 2 Viel Spaß

--- Übungscodes/Level 2/main.js
+++ Übungscodes/Level 2/main.js
@@ -1,1 +1,12 @@
+const Discord = require("discord.js")
+const bot = nw Discord.Client()
+
+bot.on( message => {
+if(message.content === "Ping") return message.reply("Pong")
+})
+
+bot.login("TOKEN")
+
+
+
 //Hier fehlt etwas findest du es?
